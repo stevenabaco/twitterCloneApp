cc7cdec8268ab2cc666ef6fb74f0433d83c56990
Steven Abaco <stevenabaco@gmail.com>
2020-10-10 11:37:51 -0500

    Add ability to show video in the tweets

--- js/index.js
+++ js/index.js
@@ -63,6 +63,7 @@ const buildTweets = (tweets, nextPage) => {
         if (tweet.extended_entities &&
             tweet.extended_entities.media.length > 0) {
             twitterContent += buildImages(tweet.extended_entities.media);
+            twitterContent += buildVideo(tweet.extended_entities.media);
         }
 
 
@@ -100,5 +101,18 @@ const buildImages = (mediaList) => {
  * Build HTML for Tweets Video
  */
 const buildVideo = (mediaList) => {
-
+    let videoContent = `<div class="tweet-video-container">`
+    let videoExists = false;
+    mediaList.map((media) => {
+        if (media.type == "video") {
+            videoExists = true;
+            videoContent += `
+            <video controls>
+                <source src="${media.video_info.variants[0].url}" type="video/mp4">
+            </video>
+            `
+        }
+    });
+    videoContent += `</div>`
+    return videoExists ? videoContent : '';
 }
